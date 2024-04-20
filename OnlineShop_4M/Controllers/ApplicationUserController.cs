66106d3ada545e49014ce5d1115b32bfc8fafd39
Kaktus200020 <artemkremnev2007@gmail.com>
2024-04-20 19:56:03 +0300

    Init

---
+++ OnlineShop_4M/Controllers/ApplicationUserController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using OnlineShop_4M_DataAccess.Data;
+using OnlineShop_4M.Models;
+using OnlineShop_4M_Models;
+using OnlineShop_4M_Utility;
+namespace OnlineShop_4M.Controllers
+{
+    public class ApplicationUserController : Controller
+    {
+        private ApplicationDbContext context;
+
+        public ApplicationUserController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+        public IActionResult Index()
+        {
+
+            //List<ApplicationUser> userList= context.ApplicationUser.Where(X=>X.Id==context.Users);
+            List<ApplicationUser> userList = null;
+
+
+
+            return View(userList);
+        }
+
+        
+        
+    }
+}
---
+++ OnlineShop_4M_Models/ShoppingCart.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace OnlineShop_4M_Models
+{
+	public class ShoppingCart
+	{
+		public int ProductId { get; set; }
+	}
+}
+
